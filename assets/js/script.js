194543235ca068713231a5757f10af5b0a89ab16
Daniel Paredes <daniel.paredes@usach.cl>
2025-07-27 10:31:06 -0400

    arreglo gitignore/boton

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,23 @@
-const alertBtn = document.getElementById("liveAlertBtn");
-const alertBox = document.getElementById("miAlerta");
 
-alertBtn.addEventListener("click", function () {
-    alertBox.classList.remove("d-none");  
-    alertBox.classList.add("show");       
-});
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder');
+    const alertTrigger = document.getElementById('liveAlertBtn');
+
+    function showAlert(message, type) {
+      // Eliminar cualquier alerta anterior
+      alertPlaceholder.innerHTML = '';
+
+      // Crear nueva alerta
+      const wrapper = document.createElement('div');
+      wrapper.innerHTML = `
+        <div class="alert alert-${type} alert-dismissible fade show" role="alert">
+          ${message}
+          <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Cerrar"></button>
+        </div>
+      `;
+
+      alertPlaceholder.append(wrapper);
+    }
+
+    alertTrigger.addEventListener('click', () => {
+      showAlert('Guardado exitosamente', 'success');
+    });
